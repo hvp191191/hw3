738ffb0a8bc5c1109c040d84dd027a10b1d61251
Harshvardhan Pandey <harsh.pandey@kellogg.northwestern.edu>
2024-02-21 23:53:09 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+   
+  # Routes for places
+    resources "places"
+    resources "entries"
+  
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
