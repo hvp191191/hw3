7821ed509fe95972fe1f317305da5ab54db3b713
Harshvardhan Pandey <harsh.pandey@kellogg.northwestern.edu>
2024-02-28 02:16:10 +0000

    Update 2

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,22 @@
 class EntriesController < ApplicationController
-  def index
-    render :template =>"entries/index"
-  end
-end
+  
+  def show
+    @entry = Entry.find_by({"id"=> params["id"]})
+    @place = Place.find_by("id" => @contact["place_id"]) 
+  end 
+   
+    def new 
+      @entry = Entry.new
+    end  
+    
+    def create 
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["posted_on"] = params["posted_on"]
+    @place = Place.find_by("name" => params["name"])
+    @entry["place_id"] = @place["id"]
+    @entry.save
+    redirect_to "/places/#{@entry["place_id"]}"
+    end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-   
+  get("/", { :controller => "places", :action => "index" })
+  get("", { :controller => "places", :action => "index" })
   # Routes for places
     resources "places"
     resources "entries"
