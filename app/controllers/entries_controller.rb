259e92fe422d13b44cef24b46ad125f5e2aa7673
Harshvardhan Pandey <harsh.pandey@kellogg.northwestern.edu>
2024-02-28 03:51:16 +0000

    update4

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,6 +6,7 @@ class EntriesController < ApplicationController
   end 
    
     def new 
+      @place = Place.find_by({"id" => params["place_id"]})
       @entry = Entry.new
     end  
     
@@ -14,8 +15,9 @@ class EntriesController < ApplicationController
     @entry["title"] = params["title"]
     @entry["description"] = params["description"]
     @entry["posted_on"] = params["posted_on"]
-    @place = Place.find_by("name" => params["name"])
-    @entry["place_id"] = @place["id"]
+    #@place = Place.find_by("name" => params["name"])
+    @entry["place_id"] = params["place_id"]
+
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
     end 
